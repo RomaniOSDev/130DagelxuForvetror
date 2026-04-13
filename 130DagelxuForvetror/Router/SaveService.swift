994b891bc8519005b9f69bc3383d0251ee21da89
Roman <romazaniosdev@gmail.com>
2026-04-13 09:43:22 +0300

    add view

---
+++ 130DagelxuForvetror/Router/SaveService.swift
@@ -0,0 +1,15 @@
+//
+//  SaveService.swift
+//  128RiprenstriFekarzor
+//
+
+
+import Foundation
+
+struct SaveService {
+    
+    static var lastUrl: URL? {
+        get { UserDefaults.standard.url(forKey: "LastUrl") }
+        set { UserDefaults.standard.set(newValue, forKey: "LastUrl") }
+    }
+}
